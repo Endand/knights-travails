1f508d58e8f23cbeb52e33f72414600d54c6195f
Endand <endand2898@gmail.com>
2024-03-18 18:58:23 -0400

    Finished knight moves method
    Now it uses BFS to find the path and keep track of parent node
    to not include the nodes not used in the path.

--- kt.rb
+++ kt.rb
@@ -1,7 +1,29 @@
 #take start and end as array of two numbers to represent coordinate
 #return the shortest t o get from start to end
 def knight_moves(start_pos,end_pos)
-   path=[start]
+   #use bfs to find shortest path
+   queue=[start_pos]
+   parents={}
+   visited=[]
+   until queue.empty?
+      curr=queue.shift
+      visited << curr
+      break if curr==end_pos
+
+      poss_moves(curr).each do |way|
+         next if visited.include?(way)
+         queue << way
+         parents[way]=curr
+      end
+   end
+   
+   shortest=[]
+   child=end_pos
+   until child.nil?
+      shortest.unshift(child)
+      child=parents[child]
+   end
+   shortest
 end
 
 #see if spot is within the board
@@ -25,4 +47,4 @@ def poss_moves(spot)
    poss
 end
 
-p poss_moves([0,0])
+p knight_moves([0,0],[3,3])
